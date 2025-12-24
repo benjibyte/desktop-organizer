9d4d9c9b5d5fcfde89712216d7c2a90f9c9c59a7
benjibyte <benjipixel@proton.me>
2025-12-24 12:55:23 -0600

    Began scripting; Ensuring the script gets all the User permissions it needs

--- src/detect_desktop.sh
+++ src/detect_desktop.sh
@@ -1,9 +1,18 @@
 # !/bin/bash
 
 # Move to the Desktop
-
+cd ~/Desktop
 # Check for a pre-existing folder structure
-
+check_folder_structure() {
+  echo "Checking for pre-existing Folder Structure..."
+  if [ -d ~/Desktop/files ]; then echo "Folders found!"
+    cd ~/Desktop/files
+    # call function to organize files from ~/Desktop to ~/Desktop/files/<groups>
+  else
+    echo "Folder Structure not found!"
+    # Ask User if they would like to generate a "files" folder to sort groups of diferent files from their desktop to.
+    # Call this function
+}
 
 # IF FOLDER STRUCTURE FOUND:
 # Read all the filenames
